89edd1c3df7acebfa59f893dcba715536cc1d865
Tom <tom.mauboussin@icloud.com>
2024-10-03 18:42:04 +0200

    #3 Création du système d'authentification

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -12,13 +12,14 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+            $table->id(); // Identifiant unique de l'utilisateur
+            $table->string('name'); // Nom de l'utilisateur
+            $table->string('email')->unique(); // Email unique
+            $table->string('password'); // Mot de passe hashé
+            $table->string('address')->nullable(); // Adresse de l'utilisateur
+            $table->string('city')->nullable(); // Ville
+            $table->string('bank_account')->nullable(); // Compte bancaire
+            $table->timestamps(); // created_at, updated_at
         });
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
