df193eaeb72ee77ec25191a4ebc394f0fb45b809
Tom <tom.mauboussin@icloud.com>
2025-02-18 10:07:14 +0100

    feat : Implémentation d'un job pour la génération de facture

---
+++ app/Jobs/GenerateBillsJob.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use App\Models\Contract;
+
+class GenerateBillsJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        $contracts = Contract::where('date_end', '>=', Carbon::now())
+                             ->where('date_start', '<=', Carbon::now())
+                             ->get();
+
+        foreach ($contracts as $contract) {
+            $contract->generateBill();
+        }
+    }
+}
